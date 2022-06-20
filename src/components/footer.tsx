0f0a10672112aa353bea9ee711c8af149616f242
Riley <riley@rileybathurst.com>
2022-06-20 15:09:21 -0700

    let them get to the multiples

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -11,13 +11,12 @@ const Footer = () => {
         <li key="multiple"><Link to="/multiple">Multiple</Link></li>
         <li key="multiple2"><Link to="/multiple2">Multiple2</Link></li>
         <li key="multiple3"><Link to="/multiple3">Multiple3</Link></li>
-        {/* <li key="multiple4"><Link to="/multiple4">Multiple4</Link></li>
+        <li key="multiple4"><Link to="/multiple4">Multiple4</Link></li>
         <li key="multiple5"><Link to="/multiple5">Multiple5</Link></li>
-        <li key="multiple6"><Link to="/multiple6">Multiple6</Link></li> */}
+        <li key="multiple6"><Link to="/multiple6">Multiple6</Link></li>
         <li key="price"><Link to="/price">Price</Link></li>
-        {/* <li key="canvas"><Link to="/canvas">Canvas</Link></li> */}
-        {/* <li key="array"><Link to="/array">Array</Link></li> */}
-        {/* <li key="array2"><Link to="/array2">Array2</Link></li> */}
+        <li key="array"><Link to="/array">Array</Link></li>
+        <li key="array2"><Link to="/array2">Array2</Link></li>
       </ul>
     </footer>
   )
