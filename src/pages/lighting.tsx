3ea54e8702c04548a1f7bd2e5ba80c29a0659c8c
Riley <riley@rileybathurst.com>
2022-12-16 18:13:06 -0800

    oops  p

--- src/pages/lighting.tsx
+++ src/pages/lighting.tsx
@@ -6,42 +6,42 @@ const LightingPage = () => {
     <main>
       <title>Lighting</title>
       <h1>Lighting</h1>
+
+      <form
+        name="contact"
+        data-netlify="true"
+        className="measure"
+        netlify-honeypot="bot-field"
+        method="POST"
+        action="/Lighting-success"
+      >
+
+        <input type="hidden" name="Lighting-name" value="contact" />
+
+        <input type="hidden" name="subject"
+          value={`Contact Lighting from sierra.lighting`} />
+
+        <label>Name
+          <input type="text" name="name" />
+        </label>
+        <label>Email
+          <input type="email" name="email" />
+        </label>
+        <label>Phone
+          <input type="tel" name="tel" />
+        </label>
+        <label>Message
+          <textarea name="message" />
+        </label>
+        <p className="sr-only">
+          <label>
+            Don&#39;t fill this out if you&#39;re human:
+            <input name="bot-field" />
+          </label>
+        </p>
+        <button type="submit">Send</button>
+      </form>
       <p>
-        <form
-          name="contact"
-          data-netlify="true"
-          className="measure"
-          netlify-honeypot="bot-field"
-          method="POST"
-          action="/Lighting-success"
-        >
-
-          <input type="hidden" name="Lighting-name" value="contact" />
-
-          <input type="hidden" name="subject"
-            value={`Contact Lighting from sierra.lighting`} />
-
-          <label>Name
-            <input type="text" name="name" />
-          </label>
-          <label>Email
-            <input type="email" name="email" />
-          </label>
-          <label>Phone
-            <input type="tel" name="tel" />
-          </label>
-          <label>Message
-            <textarea name="message" />
-          </label>
-          <p className="sr-only">
-            <label>
-              Don&#39;t fill this out if you&#39;re human:
-              <input name="bot-field" />
-            </label>
-          </p>
-          <button type="submit">Send</button>
-        </form>
-
         <Link to="/">Go home</Link>.
       </p>
     </main>
