132415ee2993e36ae6f2ccc73e53b1bc9a8f3f2f
Riley <riley@rileybathurst.com>
2022-12-16 18:01:49 -0800

    can plugins fix forms?

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -4,6 +4,34 @@ module.exports = {
   },
   plugins: [
     "gatsby-plugin-sass",
-    "gatsby-plugin-netlify"
+    "gatsby-plugin-netlify",
+    {
+      resolve: `gatsby-plugin-csp`,
+      options: {
+        mergeSecurityHeaders: true, // ? testing june 18 2022 csp issue // boolean to turn off the default security headers
+        mergeScriptHashes: false, // you can disable scripts sha256 hashes
+        mergeStyleHashes: false, // you can disable styles sha256 hashes
+        directives: {
+          "script-src": "'self' 'unsafe-inline'",
+          "style-src": "'self' 'unsafe-inline'",
+          "font-src": "'self' 'unsafe-inline' data:",
+          "img-src": "'self' data: about:",
+          "connect-src": "'self' data:"
+          // you can add your directives or override defaults
+        }
+      }
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Cut Send`,
+        short_name: `Cut Send`,
+        start_url: `/`,
+        background_color: `#fffaf2`,
+        theme_color: `#fff`,
+        display: `standalone`,
+        icon: "src/images/icon.png",
+      },
+    },
   ]
 };
