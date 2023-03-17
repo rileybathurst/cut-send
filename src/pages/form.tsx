350f31ec550a2cb6a9885d5f54df50d7a6350fed
Riley <riley@rileybathurst.com>
2023-03-17 16:09:38 -0700

    a form that cant do anything

--- src/pages/form.tsx
+++ src/pages/form.tsx
@@ -6,28 +6,28 @@ const FormPage = () => {
     <main>
       <title>Form</title>
       <h1>Form</h1>
-      {/* <p>
-        <form
-          name="form"
-        // method="POST"
-        // data-netlify="true"
-        >
-          <p>
-            <label>Your Name: <input type="text" name="name" /></label>
-          </p>
-          <p>
-            <label>Your Email: <input type="email" name="email" /></label>
-          </p>
-          <p>
-            <label>Message: <textarea name="message"></textarea></label>
-          </p>
-          <p>
-            <button type="submit">Send</button>
-          </p>
-        </form>
 
-        <Link to="/">Go home</Link>.
-      </p> */}
+      <form
+      // name="form"
+      // method="POST"
+      // data-netlify="true"
+      >
+        <p>
+          <label>Your Name: <input type="text" name="name" /></label>
+        </p>
+        <p>
+          <label>Your Email: <input type="email" name="email" /></label>
+        </p>
+        <p>
+          <label>Message: <textarea name="message"></textarea></label>
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
+      </form>
+
+      <Link to="/">Go home</Link>.
+
     </main>
   )
 }
