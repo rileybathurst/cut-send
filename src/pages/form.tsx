a2f338e067e61a2b3815aa5b51d3dfe674ffc7d2
Riley <riley@rileybathurst.com>
2023-03-17 16:05:09 -0700

    form page super simple

--- src/pages/form.tsx
+++ src/pages/form.tsx
@@ -7,18 +7,16 @@ const FormPage = () => {
       <title>Form</title>
       <h1>Form</h1>
       <p>
-        <form name="form" method="POST" data-netlify="true">
+        <form
+          name="form"
+        // method="POST"
+        // data-netlify="true"
+        >
           <p>
             <label>Your Name: <input type="text" name="name" /></label>
           </p>
           <p>
             <label>Your Email: <input type="email" name="email" /></label>
-          </p>
-          <p>
-            <label>Your Role: <select name="role[]" multiple>
-              <option value="leader">Leader</option>
-              <option value="follower">Follower</option>
-            </select></label>
           </p>
           <p>
             <label>Message: <textarea name="message"></textarea></label>
