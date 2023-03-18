05055f7542039bcab83579a771f2b4c18f0f3bf8
Riley <riley@rileybathurst.com>
2023-03-18 09:57:02 -0700

    super simple

--- src/pages/form.tsx
+++ src/pages/form.tsx
@@ -13,23 +13,13 @@ const FormPage = () => {
         // data-netlify="true"
         netlify
       >
-        <p>
-          <label>Your Name: <input type="text" name="name" /></label>
-        </p>
-        <p>
-          <label>Your Email: <input type="email" name="email" /></label>
-        </p>
-        <p>
-          <label>Message: <textarea name="message"></textarea></label>
-        </p>
-        <p>
-          <button type="submit">Send</button>
-        </p>
+        <input type="text" name="name" />
+        <button type="submit">Send</button>
       </form>
 
       <Link to="/">Go home</Link>.
 
-    </main>
+    </main >
   )
 }
 
