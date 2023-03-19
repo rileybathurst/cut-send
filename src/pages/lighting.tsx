c846adf45d68f7935c1fce0414cec3f5418226b0
Riley <riley@rileybathurst.com>
2023-03-19 10:37:36 -0700

    lighting

--- src/pages/lighting.tsx
+++ src/pages/lighting.tsx
@@ -10,40 +10,16 @@ const LightingPage = () => {
       <form
         name="contact"
         data-netlify="true"
-        className="measure"
-        netlify-honeypot="bot-field"
         method="POST"
-        action="/Lighting-success"
+        action="/success"
       >
-
-        <input type="hidden" name="Lighting-name" value="contact" />
-
-        <input type="hidden" name="subject"
-          value={`Contact Lighting from sierra.lighting`} />
-
         <label>Name
           <input type="text" name="name" />
         </label>
-        <label>Email
-          <input type="email" name="email" />
-        </label>
-        <label>Phone
-          <input type="tel" name="tel" />
-        </label>
-        <label>Message
-          <textarea name="message" />
-        </label>
-        <p className="sr-only">
-          <label>
-            Don&#39;t fill this out if you&#39;re human:
-            <input name="bot-field" />
-          </label>
-        </p>
+
         <button type="submit">Send</button>
       </form>
-      <p>
-        <Link to="/">Go home</Link>.
-      </p>
+
     </main>
   )
 }
