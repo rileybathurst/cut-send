798c32f026311fc1824fd01298b2ea968a19890c
Riley <riley@rileybathurst.com>
2023-03-19 14:10:18 -0700

    fail a test

--- src/components/hello.js
+++ src/components/hello.js
@@ -4,6 +4,6 @@ export default function Hello(props) {
   if (props.name) {
     return <h1>Hello, {props.name}!</h1>;
   } else {
-    return <div>Hey, stranger</div>;
+    return <div>Hey, fail</div>;
   }
 }
