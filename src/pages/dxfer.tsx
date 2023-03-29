23dcc6e176ff16ffe208c0a70c9709b4e9451b11
Riley <riley@rileybathurst.com>
2023-03-28 19:22:09 -0700

    dxfs

---
+++ src/pages/dxfer.tsx
@@ -0,0 +1,16 @@
+import React, { useRef, useState, useEffect } from 'react';
+
+const DxferPage = () => {
+
+  return (
+    <main>
+      <a href="http://45.79.101.19/dxfer/dxf.dxf"
+        rel='noopener noreferrer'
+      >
+        http://45.79.101.19/dxfer/dxf.dxf
+      </a>
+    </main>
+  )
+}
+
+export default DxferPage
